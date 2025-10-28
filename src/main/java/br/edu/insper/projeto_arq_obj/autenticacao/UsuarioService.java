8612dfe4225598140764124363afd75a0d812fff
felipecameloss <felipecss@al.insper.edu.br>
2025-10-28 17:43:45 -0300

    autenticação implementada Co-authored by: carloslifranco carloselfs@insper.edu.br

---
+++ src/main/java/br/edu/insper/projeto_arq_obj/autenticacao/UsuarioService.java
@@ -0,0 +1,54 @@
+package br.edu.insper.projeto_arq_obj.autenticacao;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+
+@Service
+public class UsuarioService {
+
+    private final HashMap<String, Usuario> usuarios = new HashMap<>();
+
+    private final HashMap<String, Usuario> tokens = new HashMap<>();
+
+    public Usuario cadastrarUsuario(Usuario usuario) {
+
+        String password = usuario.getPassword();
+
+        usuario.setPassword(BCrypt.hashpw(password, BCrypt.gensalt()));
+        usuarios.put(usuario.getEmail(), usuario);
+        return usuario;
+    }
+
+    public Collection<Usuario> listarUsuarios() {
+        return usuarios.values();
+    }
+
+    public String login(Usuario usuario) {
+
+        Usuario user = usuarios.get(usuario.getEmail());
+        if (BCrypt.checkpw(usuario.getPassword(), user.getPassword())) {
+
+            String token = UUID.randomUUID().toString();
+            tokens.put(token, usuario);
+            return token;
+        }
+
+        throw new RuntimeException("Usuário ou senha inválidos");
+
+    }
+
+    public void validarToken(String token) {
+
+        Usuario usuario = tokens.get(token);
+
+        if (usuario == null) {
+            throw new RuntimeException("Token invalido");
+        }
+
+    }
+
+}
---
+++ src/main/java/br/edu/insper/projeto_arq_obj/membro/model/Membro.java
@@ -0,0 +1,4 @@
+package br.edu.insper.projeto_arq_obj.membro.model;
+
+public class Membro {
+}
