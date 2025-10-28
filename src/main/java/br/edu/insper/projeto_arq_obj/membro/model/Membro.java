b99feddda9460ccfb567d50a34776ed8d3a46767
felipecameloss <felipecss@al.insper.edu.br>
2025-10-28 17:52:56 -0300

    commit da classe model dos membros Co-authored by: carloslifranco carloselfs@insper.edu.br

--- src/main/java/br/edu/insper/projeto_arq_obj/membro/model/Membro.java
+++ src/main/java/br/edu/insper/projeto_arq_obj/membro/model/Membro.java
@@ -1,4 +1,67 @@
 package br.edu.insper.projeto_arq_obj.membro.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Membro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false, unique = true)
+    private String cpf;
+    @Column(nullable = false)
+    private String ocupacao;
+
+    @ManyToMany
+    @JoinTable(
+            name = "banda_membro",
+            joinColumns = @JoinColumn(name = "id_banda"),
+            inverseJoinColumns = @JoinColumn(name = "id_membro")
+    )
+    private List<Banda> bandas;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getOcupacao() {
+        return ocupacao;
+    }
+
+    public void setOcupacao(String ocupacao) {
+        this.ocupacao = ocupacao;
+    }
+
+    public List<Banda> getBandas() {
+        return bandas;
+    }
+
+    public void setBandas(List<Banda> bandas) {
+        this.bandas = bandas;
+    }
 }
---
+++ src/main/java/br/edu/insper/projeto_arq_obj/membro/service/MembroService.java
@@ -0,0 +1,4 @@
+package br.edu.insper.projeto_arq_obj.membro.service;
+
+public class MembroService {
+}
