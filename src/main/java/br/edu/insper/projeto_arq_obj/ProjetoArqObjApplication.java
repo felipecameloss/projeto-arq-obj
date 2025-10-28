29a248e16001f90c48d49cabebbdaf1bc6c6745f
felipecameloss <felipecss@al.insper.edu.br>
2025-10-28 17:27:42 -0300

    commit do initializr do projeto

---
+++ src/main/java/br/edu/insper/projeto_arq_obj/ProjetoArqObjApplication.java
@@ -0,0 +1,13 @@
+package br.edu.insper.projeto_arq_obj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoArqObjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoArqObjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/insper/projeto_arq_obj/ProjetoArqObjApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.insper.projeto_arq_obj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoArqObjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
